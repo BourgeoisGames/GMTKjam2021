01db26f9340c2e6475beec0194292de6d4030cd1
Thomas Bonatti <thomasbonatti2695@gmail.com>
2021-06-12 14:01:32 -0400

    added help text

---
+++ Assets/Scripts/HelpText.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpText : MonoBehaviour
+{
+//	public float seconds_to_continue = 5f;
+//	public GameObject continue_text;
+	
+	
+	public string[] key_presses_to_clear;
+	private Dictionary<string, bool> keys_pressed;
+	public float auto_clear = 15f;
+	
+	void Start() {
+		keys_pressed = new Dictionary<string, bool>();
+		foreach (string key in key_presses_to_clear) {
+			keys_pressed[key] = false;
+		}
+	}
+	
+	private bool should_clear() {
+		bool should_clear_ = true;
+		foreach (KeyValuePair<string, bool> entry in keys_pressed) {
+			should_clear_ &= entry.Value;
+		}
+		return should_clear_;
+	}
+	
+	private void check_keypresses() {
+		foreach (KeyValuePair<string, bool> entry in keys_pressed) {
+			if (Input.GetKeyDown(entry.Key)) {
+				keys_pressed[entry.Key] = true;
+			}
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		check_keypresses();
+		
+		if(should_clear()) {
+			clear();
+		}
+		auto_clear -= Time.deltaTime;
+		if (auto_clear < 0) {
+			clear();
+		}
+    }
+	
+	public void clear() {
+		gameObject.SetActive(false);
+	}
+}
