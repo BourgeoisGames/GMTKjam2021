e3cdf8e76351d9da204b47f3225e4b503249df73
Jared Hoffman <jdhscripts@gmail.com>
2021-06-11 21:56:00 -0400

    enemy attacks part 1

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,17 +9,46 @@ public class EnemyController : MonoBehaviour
 	public Rigidbody body;
 	public Pathfinding pathfinding;
 
+	public float attack_speed;
+	private float attack_timer;
+	private bool can_attack;
+
+	public float attack_damage;
+	public float attack_range;
+
+	public GameObject target;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        can_attack = true;
+  		attack_timer = 0.0f;
+  		pathfinding.target = target;
+    }
+
+    public void set_target(GameObject targ)
+    {
+    	target = targ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        handle_attack();
+    }
+
+    void update_attack_availability()
+    {
+    	if(attack_timer < Time.time){
+    		can_attack = true;
+    	}
+    }
+
+    void handle_attack_cooldown()
+    {
+    	can_attack = false;
+    	attack_timer = Time.time + attack_speed;
     }
 
     void FixedUpdate()
@@ -43,5 +72,29 @@ public class EnemyController : MonoBehaviour
 
     	return to_move;
     }
+
+    void handle_attack()
+    {
+    	update_attack_availability();
+    	if(! can_attack){
+    		return;
+    	}
+
+    	if(attack_position_is_valid()){
+    		handle_attack_cooldown();
+    		do_attack();
+    	}
+    }
+
+    bool attack_position_is_valid()
+    {
+    	return false;//if()
+    }
+
+    void do_attack()
+    {
+    	return;
+    }
+
 
 }
